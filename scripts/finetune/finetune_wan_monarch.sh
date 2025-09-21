d803e70215817fd40e2a324e1bf70fcb3a54ad56
Krish Agarwal <krisha2@lovelace.ece.local.cmu.edu>
2025-09-21 18:15:32 -0400

    update

--- scripts/finetune/finetune_wan_monarch.sh
+++ scripts/finetune/finetune_wan_monarch.sh
@@ -24,10 +24,10 @@ training_args=(
 
 # Parallel arguments
 parallel_args=(
-  --num_gpus 8
+  --num_gpus 16
   --sp_size 1
   --tp_size 1
-  --hsdp_replicate_dim 8
+  --hsdp_replicate_dim 16
   --hsdp_shard_dim 1
 )
 
