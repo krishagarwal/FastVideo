6432269d810833e9da16b7b48d9f7d2105ab4a36
Krish Agarwal <krishagarwal@live.com>
2025-10-15 19:58:13 -0400

    update

--- run_job.sh
+++ run_job.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kubectl apply -f yaml/run_true_monarch_fast.yaml
-kubectl apply -f yaml/run_true_monarch.yaml
+# kubectl apply -f yaml/run_true_monarch_fast.yaml
+# kubectl apply -f yaml/run_true_monarch.yaml
 kubectl apply -f yaml/run_vsa_baseline.yaml
