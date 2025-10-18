be7cdf6d4146a45b9cb23190ac4ee1200a1382e1
Krish Agarwal <krishagarwal@live.com>
2025-10-18 15:58:38 -0400

    update

--- fastvideo/training/distillation_pipeline.py
+++ fastvideo/training/distillation_pipeline.py
@@ -1032,6 +1032,9 @@ class DistillationPipeline(TrainingPipeline):
                 log_data.update(faker_score_additional_logs)
 
                 wandb.log(log_data, step=step)
+            
+            if self.training_args.training_state_checkpointing_steps is None:
+                self.training_args.training_state_checkpointing_steps = 500
 
             # Save training state checkpoint (for resuming training)
             if (self.training_args.training_state_checkpointing_steps > 0
@@ -1049,6 +1052,9 @@ class DistillationPipeline(TrainingPipeline):
                 if self.transformer:
                     self.transformer.train()
                 self.sp_group.barrier()
+            
+            if self.training_args.weight_only_checkpointing_steps is None:
+                self.training_args.weight_only_checkpointing_steps = 200
 
             # Save weight-only checkpoint
             if (self.training_args.weight_only_checkpointing_steps > 0
