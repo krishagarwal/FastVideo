8decef5cd56f1a8d20c5ad8f06ae2f0efcb5ea4f
Krish Agarwal <krishagarwal@live.com>
2025-10-18 20:07:10 -0400

    update

--- run_job.sh
+++ run_job.sh
@@ -5,5 +5,5 @@
 # kubectl apply -f yaml/run_vsa_baseline_full_sparse.yaml
 # kubectl apply -f yaml/run_true_monarch_dmd.yaml
 # kubectl apply -f yaml/run_true_monarch_dmd_fast.yaml
-kubectl apply -f yaml/run_vsa_dmd.yaml
+# kubectl apply -f yaml/run_vsa_dmd.yaml
 kubectl apply -f yaml/run_regular_dmd.yaml
